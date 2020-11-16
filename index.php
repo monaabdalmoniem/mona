9f479f88fa1da138c49d20ac9cc92a59c3aa5968
monaabdalmoniem <74499359+monaabdalmoniem@users.noreply.github.com>
2020-11-16 18:08:40 +0200

    mona

---
+++ index.php
@@ -0,0 +1,36 @@
+<! DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+</head>
+<body bgcolor="grey">
+
+<center>
+<h1>بسم الله الرحمن الرحيم</h1>
+
+<?php
+$name="Mona AbdALmonam";
+$class="IT 3";
+$subject="PHP";
+$addess="khartoum";
+$ID_student="31013947";
+$Social_status="single";
+echo"my name is:"."$name"."<br>";
+echo"<br>";
+echo"class:"."$class"."<br>";
+echo"<br>";
+echo"subject:"."$subject"."<br>";
+echo"<br>";
+echo"addess:"."$addess"."<br>";
+echo"<br>";
+echo"ID_student:"."$ID_student"."<br>";
+echo"<br>";
+echo"Social_status:"."$Social_status"."<br>";
+
+?>
+
+</center>
+</body>
+</html>
